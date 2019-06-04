25a729040a3db2a640cfb32417abf7f7b5dbe9f7
RobFPowell <stacker@stackers-MBP.home>
2019-06-03 21:30:06 -0400

    Added cropped images and image previews

--- flask_app.py
+++ flask_app.py
@@ -16,14 +16,13 @@ s3 = boto3.client('s3')
 
 @app.route('/')
 def index(name=None):
-    # return 'Hello, World!'
     return render_template('imageHome.html', name=name)
 
 @app.route('/csvRead', methods=['GET', 'POST'])
 def csvRead():
-	# print "hello"
 	if request.method == 'POST':
 		imageList = []
+		croppedImages = []
 
 		file = request.files['pic']
 		if not file:
@@ -36,25 +35,52 @@ def csvRead():
 		for row in csv_input:
 			if rowCount > 0:
 				if len(row[3]) > 0:
-					editUpload(row[3], imageList)
+					editUpload(row[3], imageList, croppedImages)
 			rowCount += 1
-		# print imageList
-		return render_template("returnData.html", data=imageList)
-		# print imageList
-		# return ("<p>" + "</p><p>".join(imageList) + "</p>")
+
+		columnValues = zip(imageList, croppedImages)
+		return render_template("returnData.html", data=columnValues)
 
 
-def editUpload (imageUrl, imageList):
+def editUpload (imageUrl, imageList, croppedImages):
 	try:
 		response = requests.get(imageUrl, stream=True).raw
-		print 'requested image'
-		im = Image.open(response)
-		print 'Image loaded'
 
+		imCrop = Image.open(response)
+		if (imCrop.size[1] > imCrop.size[0]) or (imCrop.size[1] == imCrop.size[0]):
+			newHeightTall = int((1080 / float(imCrop.size[0])) * float(imCrop.size[1]))
+			crop = int((newHeightTall - 770)/2)
+			imCrop = imCrop.resize((1080, newHeightTall))
+			newIm = imCrop.crop((0,crop,1080,newHeightTall - crop))
+			# newImTall.show()
+		else:
+			newWideWidth = int((770 / float(imCrop.size[1])) * float(imCrop.size[0]))
+			crop = int((newWideWidth - 1080)/2)
+			imCrop = imCrop.resize((newWideWidth, 770))
+			newIm = imCrop.crop((crop,0,newWideWidth - crop,770))
+			# newImWide.show()
+
+		tempImage = StringIO()
+		newIm.save(tempImage, 'jpeg')
+
+		filename = imageUrl.rsplit("/",1)[1]
+		bucket_name = 'stacker-images'
+		s3.put_object(
+			Body=tempImage.getvalue(),
+			Bucket=bucket_name,
+			Key=filename+'cropped',
+			ACL='public-read'
+		)
+		fileUrl = 'https://s3.amazonaws.com/stacker-images/' + filename + 'cropped'
+		print fileUrl
+		croppedImages.append(fileUrl)
+
+		response = requests.get(imageUrl, stream=True).raw
+
+		im = Image.open(response)
 		blurred = im.filter(ImageFilter.GaussianBlur(25))
 		blurred = blurred.resize((1080, 770))
 
-		print "Blur worked"
 		if im.size[0] == im.size[1]:
 			newWidth = 770
 			newHeight = 770
@@ -71,8 +97,6 @@ def editUpload (imageUrl, imageList):
 			im = im.resize((newWidth,int(newHeight)))
 			offset = (0, int((blurred.size[1] - int(newHeight)) // 2))
 
-		print newWidth, newHeight
-		print offset
 		blurred.paste(im, offset)
 
 		blurred1 = StringIO()
@@ -80,25 +104,16 @@ def editUpload (imageUrl, imageList):
 
 		filename = imageUrl.rsplit("/",1)[1]
 		bucket_name = 'stacker-images'
-		# s3.put_object(
-		# 	Body=blurred1.getvalue(),
-		# 	Bucket=bucket_name,
-		# 	Key=filename,
-		# 	ACL='public-read'
-		# )
+		s3.put_object(
+			Body=blurred1.getvalue(),
+			Bucket=bucket_name,
+			Key=filename,
+			ACL='public-read'
+		)
 		fileUrl = 'https://s3.amazonaws.com/stacker-images/' + filename
 		print fileUrl
 		imageList.append(fileUrl)
-		# print imageList
-		# print fileUrl
+
 	except:
 		print 'Error - ' + imageUrl
-		imageList.append('Error - ' + imageUrl)
-		# pass
-
-# def uploadImage ():
-# 	filename = 'Your_Highness_Poster.jpg'
-# 	bucket_name = 'stacker-images'
-# 	s3.upload_file(filename, bucket_name, filename, ExtraArgs={'ACL':'public-read'})
-# 	fileUrl = 'https://s3.amazonaws.com/stacker-images/' + filename
-
+		imageList.append(imageUrl)
